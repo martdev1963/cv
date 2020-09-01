80808bdb0db8dcec81eb2bf31f4d397aa5de6c25
Martin Batista - Full Stack Developer <martdev1963@gmail.com>
2020-09-01 10:38:35 -0400

    added sounds to each if conditional...
    added sounds to each if conditional...

--- index.js
+++ index.js
@@ -15,17 +15,24 @@ var randomNumber2  = Math.floor(Math.random() * 6) + 1;
 
 var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
 
+var player1Sound = new Audio("success.mp3");
+var player2Sound = new Audio("opponent.mp3");
+var drawSound = new Audio("draw.mp3");
+
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
 
 if (randomNumber1 > randomNumber2) {
   document.querySelector("h1").innerHTML = "🤠Player 1 Wins!!";
+  player1Sound.play();
 }
 else if (randomNumber2 > randomNumber1) {
   document.querySelector("h1").innerHTML = "😀Player 2 Wins!!";
+  player2Sound.play();
 }
 else {
   document.querySelector("h1").innerHTML = "🌞DRAW!!";
+  drawSound.play();
 }
 
 function rollTheDice(){
